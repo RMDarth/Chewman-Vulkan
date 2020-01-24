9da6f3926300a612ba985d7e3b437fe416fb6ca4
RM_Darth <darthigor@mail.ru>
2020-01-24 03:23:43 +0200

    Ceiling/wall material configuration

--- Game/Level/GameMapLoader.cpp
+++ Game/Level/GameMapLoader.cpp
@@ -433,6 +433,7 @@ void GameMapLoader::initMeshes(GameMap& level, const std::string& suffix)
 
                     auto wallTop = std::make_shared<SVE::MeshEntity>("WallTop");
                     wallTop->setMaterial("CeilingNormals" + skinId);
+                    wallTop->getMaterialInfo()->diffuse = {0.85f, 0.85f, 0.85f, 1.0f};
                     node->attachEntity(std::move(wallTop));
                     auto wallVert = std::make_shared<SVE::MeshEntity>("WallVert");
                     wallVert->setMaterial("WallNormalsInstanced" + skinId);
@@ -451,6 +452,7 @@ void GameMapLoader::initMeshes(GameMap& level, const std::string& suffix)
 
     level.floor[0] = std::make_shared<SVE::MeshEntity>("AllFloorTop" + suffix);
     level.floor[1] = std::make_shared<SVE::MeshEntity>("AllFloorVert" + suffix);
+    level.floor[0]->getMaterialInfo()->diffuse = {1.05f, 1.05f, 1.05f, 1.0f};
     auto floorNode = SVE::Engine::getInstance()->getSceneManager()->createSceneNode();
     level.mapNode->attachSceneNode(floorNode);
     floorNode->attachEntity(level.floor[0]);
